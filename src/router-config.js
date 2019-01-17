f3a578966020c5c1dd536924260ab158ea828938
huangs <huangs@gidomino.com>
2019-01-17 18:32:10 +0800

    modify router

--- src/router-config.js
+++ src/router-config.js
@@ -3,6 +3,11 @@ import Router from 'vue-router'
 // import login1 from './views/login1'
 import login from './views/login'
 import mapGI from './views/mapGI'
+import linkInfo from './views/linkInfo'
+import linkMa from './views/linkMa'
+import moreInfo from './views/moreInfo'
+import ddd from './views/ddd'
+
 
 
 Vue.use(Router)
@@ -21,6 +26,26 @@ export default new Router({
       path: '/mapGI',
       name: 'mapGI',
       component: mapGI
+    },
+    {
+      path: '/b',
+      name: 'linkInfo',
+      component: linkInfo
+    },
+    {
+      path: '/a',
+      name: 'linkMa',
+      component: linkMa
+    },
+    {
+      path: '/c',
+      name: 'moreInfo',
+      component: moreInfo
+    },
+    {
+      path: '/d',
+      name: 'd',
+      component: ddd
     },
   ]
 })
