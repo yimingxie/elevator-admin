c12ca126ed2e2c4a25994435e3000151d38ea281
xieyiming <329715712@qq.com>
2019-04-27 11:33:23 +0800

    V1.6.7.5

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
   <div id="page-container">
     <div id="header">
+      <div class="header-logo">
+        <img src="./assets/images/unit_logo.png" alt="">
+      </div>
       {{ $route.meta.title }}
     </div>
     <span class="right_header">
       <!-- <i class="headPic"></i> -->
-      <span>版本号V1.6.7.4</span>
+      <!-- 只有详情页才展示 -->
+      <span v-if="$route.meta.title == '电梯运行详情'"><img src="./assets/images/power.png" alt=""><i>|</i></span>
+      <span>版本号V1.6.7.5</span>
       <!-- <i class="split"></i>
       <i class="quit"></i> -->
     </span>
     
     <router-view></router-view>
+
+    <!-- 此处详情页不展示 -->
+    <div class="power-by" v-if="$route.meta.title !== '电梯运行详情'">
+      <img src="./assets/images/power.png" alt="">
+    </div>
+
+
   </div>
 </template>
 
@@ -24,7 +36,7 @@
     },
     
     mounted () {
-      // console.log(this.$route.meta.title)
+      // console.log(this.$router.meta.title)
       // document.title = this.$route.meta.title
 
     },
@@ -47,6 +59,7 @@
   width: 100%;
   height: 100%;
   #header{
+    position relative
     size 100% 80px
     line-height 80px
     background: #16161C;
@@ -57,10 +70,22 @@
     text-align: center;
     fixed top 0
     z-index 999
+
+    .header-logo{
+      position absolute;
+      top 0;
+      left 24px;
+    }
+  }
+  .power-by{
+    position fixed
+    bottom 26px
+    left 24px
+
   }
   .right_header{
     z-index 999
-    size 220px 80px 
+    size auto 80px 
     font-size 0
     line-height 80px
     text-align right
@@ -77,7 +102,20 @@
     span{
       font-size: 14px;
       color: #66667F;
-      margin-left 20px
+      margin-left 10px
+
+      i{
+        display inline-block
+        margin-left 12px;
+        font-weight bold
+        vertical-align middle
+        margin-top -4px;
+      }
+
+      img{
+        margin-top -2px
+        vertical-align middle
+      }
     }
     .split {
       display: inline-block;
